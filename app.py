3f2a7b95a80c71fb7a7701b74c27f2a38453454b
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-09-16 06:27:33 +0100

    add html template

--- app.py
+++ app.py
@@ -1,38 +1,13 @@
-from flask import Flask, request, make_response
-app =Flask(__name__)
+from flask import Flask, render_template
+app =Flask(__name__ , template_folder='templates')
 
 @app.route('/')
 def index():
-    return '<h1>First flask</h1>'
+    myvalue = 'Nourpow'
+    myresult = 9+10
+    mylist=[10,20,30,40,50]
+    return render_template('index.html', myvalue=myvalue, myresult=myresult, mylist=mylist)
 
-@app.route('/hello' )
-def hello():
-    response= make_response('Hey there!')
-    response.status_code = 201
-    response.headers['content-type'] = 'text/plain'
-    
-    return response
-    
-
-@app.route('/greet/<name>')
-def greet(name):
-    return f'Hello {name}'
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1} + {number2} = {number1 + number2}'
-
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f'{greeting} , {name}'
-    else:
-        return 'Some parameters are missing'
-    
-    
-    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5555, debug=True)
