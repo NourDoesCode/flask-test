7a81f181b4c5b0e2a19329522423c55e295d633d
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-09-16 16:42:49 +0100

    add login  forum

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app =Flask(__name__ , template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    myvalue = 'Nourpow'
-    myresult = 9+10
-    mylist=[10,20,30,40,50]
-    return render_template('index.html', myvalue=myvalue, myresult=myresult, mylist=mylist)
+    if request.method=='GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username=request.form.get('username')
+        password=request.form.get('password')
+        if username == 'nour' and password =='123456':
+            return 'success!'
+        else: return 'failed!'
 
 
 if __name__ == '__main__':
